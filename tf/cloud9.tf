93bc1ffae8f129caaa034e71a9d0af6bc636ba85
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2023-08-24 11:55:03 +0900

    Create cloud9.tf

---
+++ tf/cloud9.tf
@@ -0,0 +1,12 @@
+# https://spak.no/blog/article/61407c2e77201a55fc6dc87b
+
+resource "aws_cloud9_environment_ec2" "cloud9_instance" {
+  name                        = "eks_cloud9"
+  instance_type               = "t2.medium"
+  automatic_stop_time_minutes = 30
+  subnet_id                   = 
+
+  tags = {
+    Terraform = "true"
+  }
+}
