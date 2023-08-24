a06ab873f5d57b5d397670cd81282d7ce04e3cc2
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2023-08-24 10:55:10 +0900

    Update var.tf

--- tf/var.tf
+++ tf/var.tf
@@ -4,10 +4,10 @@
     $ terraform destroy
 */
 
-variable "your_ip_addr" {
-    type = string
-    default = "218.238.107.0/24"       ## 네이버에서 "내아이피" 로 검색한 후, 결과값을 CIDR 형태로 입력.
-}
+#variable "your_ip_addr" {
+#    type = string
+#    default = "218.238.107.0/24"       ## 네이버에서 "내아이피" 로 검색한 후, 결과값을 CIDR 형태로 입력.
+#}
 
 variable "key_pair" {
     type = string
