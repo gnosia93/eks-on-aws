e159a07b70d1eb870c4b68abe283d91207e5c0b3
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2023-09-03 07:44:07 +0900

    Update redis.tf

--- tf/redis.tf
+++ tf/redis.tf
@@ -3,7 +3,7 @@ resource "aws_elasticache_cluster" "eks_redis" {
   cluster_id           = "eks-redis"
   engine               = "redis"
   engine_version       = "7.0.7"
-  node_type            = "cache.m3.medium"
+  node_type            = "cache.t3.small"
   num_cache_nodes      = 1
   parameter_group_name = "default.redis7.cluster.on"
   port                 = 6379
