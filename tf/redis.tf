757d08fc6394ba6f5a92599c32549bed57bc419c
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2023-09-03 07:48:59 +0900

    Update redis.tf

--- tf/redis.tf
+++ tf/redis.tf
@@ -7,4 +7,11 @@ resource "aws_elasticache_cluster" "eks_redis" {
   num_cache_nodes      = 1
   parameter_group_name = "default.redis7.cluster.on"
   port                 = 6379
+  apply_immediately    = "true"
+  subnet_group_name    = ...
+}
+
+resource "aws_elasticache_subnet_group" "eks_redis_subnet_grp" {
+  name       = "eks-redis-subnet-grp"
+  subnet_ids = [aws_subnet.foo.id]
 }
