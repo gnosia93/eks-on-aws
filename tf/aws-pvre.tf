a5e75eff0b780a87847d55aacdbc646657d647b8
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2023-09-12 10:00:14 +0900

    Update aws-pvre.tf

--- tf/aws-pvre.tf
+++ tf/aws-pvre.tf
@@ -4,6 +4,6 @@ data "aws_iam_policy" "pvre_policy" {
 
 // ec2
 resource "aws_iam_role_policy_attachment" "pvre-attach-ec2" {
-  role       = aws_iam_role.bigdata_ec2_service_role.name
+  role       = aws_iam_role.eks_ec2_service_role.name
   policy_arn = data.aws_iam_policy.pvre_policy.arn
 }
