787aee019abe6afa52b4e78f308a0563636e2fee
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2023-09-19 08:25:56 +0900

    Update aws-pvre.tf

--- tf/aws-pvre.tf
+++ tf/aws-pvre.tf
@@ -2,8 +2,20 @@ data "aws_iam_policy" "pvre_policy" {
     arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-// ec2
+data "aws_iam_policy" "pvre_policy_patch" {
+    arn = "arn:aws:iam::aws:policy/AmazonSSMPatchAssociation"
+}
+
+// ec2 SSM Core
 resource "aws_iam_role_policy_attachment" "pvre-attach-ec2" {
   role       = aws_iam_role.eks_ec2_service_role.name
   policy_arn = data.aws_iam_policy.pvre_policy.arn
 }
+
+
+// ec2 SSM Patch
+resource "aws_iam_role_policy_attachment" "pvre-attach-patch-ec2" {
+  role       = aws_iam_role.eks_ec2_service_role.name
+  policy_arn = data.aws_iam_policy.pvre_policy_patch.arn
+}
+
